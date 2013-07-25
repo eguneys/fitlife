e1631ab399398391f70a25242f41fcfa3951887b
eguneys <eguneys@hotmail.com>
2013-07-25 17:40:44 +0000

    New template added complete-admin

--- web.js
+++ web.js
@@ -5,9 +5,25 @@ var app = express();
 
 /* Serve static content */
 app.configure(function () {
+    app.set('view engine', 'jade');
     app.use(express.static(__dirname + "/public"));
 });
 
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/Progress', function(req, res) {
+    res.render('progress');
+});
+
+app.get('/Nutrition', function(req, res) {
+    res.render('nutrition');
+});
+
+app.get('/Activity', function(req, res) {
+    res.render('activity2');
+});
 
 app.listen(8080, function () {
     console.log(" - listening on 8080 ");
